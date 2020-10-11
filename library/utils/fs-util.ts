c45735350a4647959eb65e389f83d579fcc6885c
sdegetaus <santiago.degetau@gmail.com>
2020-10-11 08:32:39 -0600

    Fix fs functions and added recurisive copyAll function

--- library/utils/fs-util.ts
+++ library/utils/fs-util.ts
@@ -2,31 +2,33 @@ import fs from "fs";
 import path from "path";
 
 /**
- * Remove all files and sub-directories in a directory. Adapted from @guybedford's code:
+ * Recursively remove all files and sub-directories in a directory.
+ * Adapted from @guybedford's code:
  * https://gist.github.com/liangzan/807712/8fb16263cb39e8472d17aea760b6b1492c465af2#gistcomment-337828
- * @param dirPath
  */
 const removeDirContent = (dirPath: string) => {
   try {
-    const files = fs.readdirSync(dirPath);
-    if (files.length > 0) {
-      for (let i = 0; i < files.length; i++) {
-        var filePath = path.join(dirPath, files[i]);
-        if (fs.statSync(filePath).isFile()) {
-          fs.unlinkSync(filePath);
-        } else {
-          removeDirContent(filePath);
-        }
+    fs.readdirSync(dirPath).forEach((file) => {
+      const filePath = path.join(dirPath, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isFile()) {
+        fs.unlinkSync(filePath);
+      } else if (stat.isDirectory()) {
+        removeDirContent(filePath);
+        fs.rmdirSync(filePath);
+      } else {
+        console.info(
+          `Skipping ${filePath} because it is neither a file or a directory.`
+        );
       }
-    }
+    });
   } catch (e) {
     throw e;
   }
 };
 
 /**
- * Synchronously create directory if it doesn't exist
- * @param dirPath
+ * Synchronously create directory if it doesn't exist.
  */
 const ensureDirSync = (dirPath: string) => {
   try {
@@ -39,7 +41,32 @@ const ensureDirSync = (dirPath: string) => {
 };
 
 /**
- * Get the root path
+ * Recursively copy all files and directories.
+ */
+const copyAll = (srcDir: string, destDir: string) => {
+  try {
+    fs.readdirSync(srcDir).forEach((file) => {
+      const filePath = path.join(srcDir, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isFile()) {
+        fs.copyFileSync(filePath, path.join(destDir, file));
+      } else if (stat.isDirectory()) {
+        const subDestDir = path.join(destDir, file);
+        ensureDirSync(subDestDir);
+        copyAll(filePath, subDestDir);
+      } else {
+        console.info(
+          `Skipping ${filePath} because it is neither a file or a directory.`
+        );
+      }
+    });
+  } catch (e) {
+    throw e;
+  }
+};
+
+/**
+ * Get the root path (where the root `node_modules` are!)
  * https://stackoverflow.com/a/57102773
  */
 const getRootPath = () => {
@@ -48,4 +75,4 @@ const getRootPath = () => {
   return regexResp ? regexResp[1] : fullPath;
 };
 
-export default { removeDirContent, ensureDirSync, getRootPath };
+export default { removeDirContent, ensureDirSync, getRootPath, copyAll };
